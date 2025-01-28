83687296ab7edd200e667db910574d506921b094
Abdu Jemal <abdu1994jemal@gmail.com>
2025-01-28 22:15:46 +0300

    done

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import path from 'path';
 // const exphbs = require('express-handlebars');
 import bodyparser from 'body-parser';
 // import courseController from './controller/course_controller';
-import {formatJson, saveJson, uploadAllCourses, uploadFromJson, uploadLatestCourses} from './firebase.js';
+// import {formatJson, saveJson, uploadAllCourses, uploadFromJson, uploadLatestCourses} from './firebase.js';
 import {connectToMongo} from './models/db.js'
 import CourseModel from './models/course.model.js';
 
