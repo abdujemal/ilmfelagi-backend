de042544696d46a417dbaeef97159bd20683acab
Abdu Jemal <abdu1994jemal@gmail.com>
2025-01-29 15:41:03 +0300

    bug fix index

--- controller/routes.js
+++ controller/routes.js
@@ -1,7 +1,14 @@
 import express from 'express';
-import { getLatestCourses } from './course_controller';
+import { getLatestCourses, searchCourses } from './course_controller.js';
+import { addCourse, deleteCourse, updateCourse, getAllCourses } from './course_controller.js';
 var router = express.Router();
 
 router.get('/', getLatestCourses);
+router.post('/add', addCourse);
+router.post('/delete', deleteCourse);
+router.post('/update', updateCourse);
+router.get('/getAll', getAllCourses);
+router.get('/search', searchCourses);
+
 
 export default router;
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from 'express';
 import path from 'path';
 // const exphbs = require('express-handlebars');
 import bodyparser from 'body-parser';
-import courseController from './controller/course_controller.js';
+import routes from './controller/routes.js';
 // import {formatJson, saveJson, uploadAllCourses, uploadFromJson, uploadLatestCourses} from './firebase.js';
 import {connectToMongo} from './models/db.js'
 import CourseModel from './models/course.model.js';
@@ -17,7 +17,7 @@ app.use(bodyparser.json());
 // app.set('views', path.join(__dirname, '/views/'));
 // app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
 // app.set('view engine', 'hbs');
-app.use('/api/courses', courseController);
+app.use('/api/courses', routes);
 
 app.listen(3000, () => {
 
