97daec36c3f469b33fb9ea966504c47cbbda1740
Abdu Jemal <abdu1994jemal@gmail.com>
2025-01-29 17:28:10 +0300

    async approach

--- controller/course_controller.js
+++ controller/course_controller.js
@@ -1,38 +1,48 @@
 import express from 'express';
 import CourseModel from '../models/course.model.js';
 
+const router = express.Router();
+
 export const getLatestCourses =  async(req,res)=>{
+    try{
 
-    const courses = await CourseModel.find({ dateTime: { $gt: req.query.dateTime } });
-
-    res.status(201).json(courses);
+        const courses = await CourseModel.find({ dateTime: { $gt: req.query.dateTime } });
+    
+        res.status(201).json(courses);
+    } catch (e) {
+        res.status(500).json({ msg: e.message });
+    }
 };
-
+export const getCourseById = async (req, res) => {
+    try {
+        const course = await CourseModel.findById(req.params.id);
+        if (!course) {
+            return res.status(404).json({ msg: "Course not found" });
+        }
+        res.status(200).json(course);
+    } catch (e) {
+        res.status(500).json({ msg: e.message });
+    }
+};
 export const addCourse = async (req, res) => {    
     if(!req.body.dateTime && !req.body.courseId){
         res.status(500).json({msg: "no data"})
     }
 
-    const model = await CourseModel.findOne({courseId: req.body.courseId});
+    try {
+        const model = await CourseModel.findOne({courseId: req.body.courseId});
 
-    if(model){
-        CourseModel.findOneAndUpdate({courseId: req.body.courseId}, req.body).then(()=>{
-        res.status(200).json({msg: `Course updated`})
-        }).catch((e)=>{
-        res.status(500).json({msg: e})
-        })      
-        
-    }else{
-        const courseModel = new CourseModel(
-            req.body,
-        );
-    
-        courseModel.save().then(()=>{
+        if(model){
+            await CourseModel.findOneAndUpdate({courseId: req.body.courseId}, req.body);
+            res.status(200).json({msg: `Course updated`});
+        } else {
+            const courseModel = new CourseModel(req.body);
+            await courseModel.save();
             res.status(200).json({msg: "Successfully created"}); 
-        }).catch((err)=>{
-            res.status(500).json({msg: err});
-        })
-    }  
+        }
+    } catch (e) {
+        res.status(500).json({msg: e.message});
+    }
 };
 
 export const deleteCourse = async (req, res) => {
@@ -40,54 +50,131 @@ export const deleteCourse = async (req, res) => {
         res.status(500).json({msg: "no data"})
     }
 
-    CourseModel.updateOne({ courseId: req.body.courseId },
-        { $set: { isDeleted: true } }, // Use $set to add the new field with its value
-    { new: true, merge: true } // Options to return the updated document and merge the new field with existing fields
-        ).then(() => { 
-            res.status(201).json({msg: "Successfully Deleted"}); 
-        }).catch((err)=>{
-            res.status(500).json({msg: err});
-        });
-     
+    try {
+        await CourseModel.updateOne({ courseId: req.body.courseId },
+            { $set: { isDeleted: true } }, 
+            { new: true, merge: true }
+        );
+        res.status(201).json({msg: "Successfully Deleted"}); 
+    } catch (e) {
+        res.status(500).json({msg: e.message});
+    }
 };
 
 export const searchCourses = async (req, res) => {
     if(!req.query.q){
         res.status(400).json({msg: "no date found"})
-      }
-    
-    
-    CourseModel.find({
+    }
+
+    try {
+        const courses = await CourseModel.find({
             title: { $regex: req.query.q, $options: 'i' }
-            }).then((courses)=>{
+        });
         res.status(200).json(courses);
-    }).catch((e)=>{
-        res.status(500).json({err: e})
-    })
-    
+    } catch (e) {
+        res.status(500).json({err: e.message});
+    }
 };
 
 export const updateCourse = async (req, res) => {
     if(req.method !== 'POST'){
         res.status(400).json({msg: "access denied!"})
     }
-    
+
     if(!req.body.dateTime || !req.body.courseId){
         res.status(500).json({msg: "no data"})
     }
     if(!req.query.userName || !req.query.password){
         res.status(400).json({msg: "access denied!"})
     }
-    
-    CourseModel.updateOne({ courseId: req.body.courseId }, // Query condition to match the document
-    { $set: req.body }).then(() => {
-        
+
+    try {
+        await CourseModel.updateOne({ courseId: req.body.courseId }, { $set: req.body });
         res.status(201).json({msg: "Successfully updated"}); 
-        
-    }).catch((err)=>{
-        res.status(500).json({msg: err});
-    });
+    } catch (e) {
+        res.status(500).json({msg: e.message});
+    }
 };
+// export const addCourse = async (req, res) => {    
+//     if(!req.body.dateTime && !req.body.courseId){
+//         res.status(500).json({msg: "no data"})
+//     }
+
+//     const model = await CourseModel.findOne({courseId: req.body.courseId});
+
+//     if(model){
+//         CourseModel.findOneAndUpdate({courseId: req.body.courseId}, req.body).then(()=>{
+//         res.status(200).json({msg: `Course updated`})
+//         }).catch((e)=>{
+//         res.status(500).json({msg: e})
+//         })      
+        
+//     }else{
+//         const courseModel = new CourseModel(
+//             req.body,
+//         );
+    
+//         courseModel.save().then(()=>{
+//             res.status(200).json({msg: "Successfully created"}); 
+//         }).catch((err)=>{
+//             res.status(500).json({msg: err});
+//         })
+//     }  
+// };
+
+// export const deleteCourse = async (req, res) => {
+//     if(!req.body.courseId){
+//         res.status(500).json({msg: "no data"})
+//     }
+
+//     CourseModel.updateOne({ courseId: req.body.courseId },
+//         { $set: { isDeleted: true } }, // Use $set to add the new field with its value
+//     { new: true, merge: true } // Options to return the updated document and merge the new field with existing fields
+//         ).then(() => { 
+//             res.status(201).json({msg: "Successfully Deleted"}); 
+//         }).catch((err)=>{
+//             res.status(500).json({msg: err});
+//         });
+     
+// };
+
+// export const searchCourses = async (req, res) => {
+//     if(!req.query.q){
+//         res.status(400).json({msg: "no date found"})
+//       }
+    
+    
+//     CourseModel.find({
+//             title: { $regex: req.query.q, $options: 'i' }
+//             }).then((courses)=>{
+//         res.status(200).json(courses);
+//     }).catch((e)=>{
+//         res.status(500).json({err: e})
+//     })
+    
+// };
+
+// export const updateCourse = async (req, res) => {
+//     if(req.method !== 'POST'){
+//         res.status(400).json({msg: "access denied!"})
+//     }
+    
+//     if(!req.body.dateTime || !req.body.courseId){
+//         res.status(500).json({msg: "no data"})
+//     }
+//     if(!req.query.userName || !req.query.password){
+//         res.status(400).json({msg: "access denied!"})
+//     }
+    
+//     CourseModel.updateOne({ courseId: req.body.courseId }, // Query condition to match the document
+//     { $set: req.body }).then(() => {
+        
+//         res.status(201).json({msg: "Successfully updated"}); 
+        
+//     }).catch((err)=>{
+//         res.status(500).json({msg: err});
+//     });
+// };
 
 export const getAllCourses = async (req, res) => {
     try {
